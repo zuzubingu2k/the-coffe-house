d21af5b20f4430ee2f85021ff897759b7e5a762a
Cong <congnguyen26082000@gmail.com>
2021-08-16 23:55:46 +0700

    flast list home

--- components/ListHome.js
+++ components/ListHome.js
@@ -1,10 +1,56 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, FlatList, Image } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome';
 export default function ListHome() {
+    const DATA = [
+        {
+            id: '58694a0f-3da1-471f-bd96-145571e29d72',
+            image: 'https://ngocdenroi.com/wp-content/uploads/2015/09/the-coffee-house-1441102961552-crop-1441123481946.jpg',
+            "address": ' 641 Phạm Văn Đồng ,Bắc Từ Liêm,Hà Nội',
+        },
+        {
+            id: '58694a0f-3da1-471f-bd96-145571e29d72',
+            image: 'https://bloganchoi.com/wp-content/uploads/2019/12/ha-dong.jpg',
+            "address": ' 641 Phạm Văn Đồng ,Bắc Từ Liêm,Hà Nội',
+        },
+        {
+            id: '58694a0f-3da1-471f-bd96-145571e29d72',
+            image: 'https://file.hstatic.net/1000075078/file/1212-tch-p5_8ca0d2d9507a42c69fb5c8b3591d6787_1024x1024.jpg',
+            "address": ' 641 Phạm Văn Đồng ,Bắc Từ Liêm,Hà Nội',
+        },
+        {
+            id: '58694a0f-3da1-471f-bd96-145571e29d72',
+            image: 'https://file.hstatic.net/1000075078/file/tch-hoang_dao_thuy_a04a726e631e44c8a6e513c1e7ab3afa_1024x1024.jpg',
+            "address": ' 641 Phạm Văn Đồng ,Bắc Từ Liêm,Hà Nội',
+        },
+        {
+            id: '58694a0f-3da1-471f-bd96-145571e29d72',
+            image: 'https://cafebiz.cafebizcdn.vn/zoom/700_438/162123310254002176/2021/3/13/photo1615650312715-16156503159861438233881.jpg',
+            "address": ' 641 Phạm Văn Đồng ,Bắc Từ Liêm,Hà Nội',
+        },
+
+    ]
+    const renderItem = ({ item }) => (
+        <View style={{ backgroundColor: 'white', width: '90%', height: 130, marginLeft: 18, borderRadius: 15, marginBottom: 5 }}>
+            <TouchableOpacity style={{ flexDirection: 'row', marginTop: 15 }}>
+                <View style={styles.item}>
+                    <Image
+                        style={{ height: 90, width: 90, padding: 20, marginLeft: 10 }}
+                        source={{
+                            uri: item.image,
+                        }}
+                    />
+                </View>
+                <View style={{ marginLeft: 20, marginTop: 15 }}>
+                    <Text>THE COFFE HOUSE</Text>
+                    <Text style={{ width: 260, fontSize: 13 }}>{item.address}</Text>
+                </View>
+            </TouchableOpacity>
+        </View>
+    )
     return (
         <SafeAreaView style={styles.container}>
-            <View style={{ flexDirection: 'row', marginTop: 10 }}>
+            <View style={{ flexDirection: 'row', marginTop: 10, marginLeft: 10 }}>
                 <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Cửa Hàng</Text>
                 <View style={{ flexDirection: 'row' }}>
                     <View>
@@ -19,16 +65,31 @@ export default function ListHome() {
                     </View>
                 </View>
             </View>
-            <View style={{ flexDirection: 'row' }}>
+            <View style={{ flexDirection: 'row', marginTop: 10 }}>
                 <TextInput
                     style={styles.input}
                     placeholder="Nhập tên đường,quận.."
-                    placeholderTextColor="#a1a8b0"
+                    placeholderTextColor="black"
                 />
-                <TouchableOpacity>
+                <TouchableOpacity style={{ flexDirection: 'row', marginLeft: 10, marginTop: 15 }}>
                     <Icon style={{ textAlign: 'center', top: 5 }} name="list" size={20} color="#aaa" />
+                    <Text style={{ marginLeft: 10 }}>Bản đồ</Text>
                 </TouchableOpacity>
             </View>
+            <ScrollView>
+                <View style={{ height: 1500, backgroundColor: '#ddd' }}>
+                    <View style={{ marginLeft: 20, marginTop: 30, height: 40 }}>
+                        <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Các cửa hàng khác</Text>
+                    </View>
+                    <FlatList
+                        data={DATA}
+                        renderItem={renderItem}
+                        keyExtractor={item => item.id}
+                        numColumns={1}
+
+                    />
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
@@ -39,10 +100,13 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     input: {
+        color: 'black',
+        borderRadius: 10,
+        width: "70%",
         height: 40,
         margin: 12,
         borderWidth: 1,
         padding: 10,
-
+        backgroundColor: '#d8d6d6'
     },
 })
